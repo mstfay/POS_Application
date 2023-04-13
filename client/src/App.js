eb36421a35f756b2f876437cbaddfebd0496eab4
mstfay <aymustafa.32@gmail.com>
2023-04-13 23:55:34 +0300

    Starting with MERN

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import { Button } from "antd";
+
+function App() {
+  return (
+    <div>
+      <Button type="primary">Primary Button</Button>
+      <h1 class="text-3xl font-bold underline text-red-600">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
