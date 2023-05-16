9213499e710a467eeaf69fdc62f569ac31bde56c
mstfay <aymustafa.32@gmail.com>
2023-05-16 16:33:35 +0300

    The process of pulling categories from the database has been completed.

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,16 +1,33 @@
-import React from "react";
-import Header from "../components/Header/Header"
+import React, { useState, useEffect } from "react";
+import Header from "../components/Header/Header";
 import Categories from "../components/Categories/Categories";
 import Products from "../components/Products/Products";
 import CartTotals from "../components/Cart/CartTotals";
 
 function HomePage() {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const getCategories = async () => {
+      try {
+        const response = await fetch(
+          "http://localhost:5000/api/categories/get-all"
+        );
+        const data = await response.json();
+        setCategories(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getCategories();
+  }, []);
+
   return (
     <React.Fragment>
       <Header />
       <div className="home px-6 flex md:flex-row flex-col justify-between gap-10 md:pb-0 pb-24">
         <div className="categories overflow-auto max-h-[calc(100vh_-_112px)] md:pb-10">
-          <Categories />
+          <Categories categories={categories} setCategories={setCategories}/>
         </div>
         <div className="products flex-[8] overflow-auto max-h-[calc(100vh_-_112px)] pb-10">
           <Products />
