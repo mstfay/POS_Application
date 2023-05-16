c834734e5f801af531aedfcb6fadbfe9f1664648
mstfay <aymustafa.32@gmail.com>
2023-05-16 16:11:32 +0300

    The process of sending categories to the database has been completed.

--- client/src/components/Categories/Categories.jsx
+++ client/src/components/Categories/Categories.jsx
@@ -1,37 +1,84 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./style.css";
+import { Button, Form, Input, Modal, message } from "antd";
+import { PlusOutlined } from "@ant-design/icons";
 
 const Categories = () => {
-  return (
-    <ul className='flex gap-4 md:flex-col text-lg'>
-        <li className='bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]'>
-            <span>Tümü</span>
-        </li>
-        <li className='bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]'>
-            <span>Yiyecek</span>
-        </li>
-        <li className='bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]'>
-            <span>İçecek</span>
-        </li>
-        <li className='bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]'>
-            <span>İçecek</span>
-        </li>
-        <li className='bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]'>
-            <span>İçecek</span>
-        </li>
-        <li className='bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]'>
-            <span>İçecek</span>
-        </li>
-        <li className='bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]'>
-            <span>İçecek</span>
-        </li>
-        <li className='bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]'>
-            <span>İçecek</span>
-        </li>
-        <li className='bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]'>
-            <span>İçecek</span>
-        </li>
-    </ul>
-  )
-}
+  const [isAddModalOpen, setIsAddModalOpen] = useState(false);
+  const [form] = Form.useForm();
 
-export default Categories
+  const onFinish = (values) => {
+    try {
+      fetch("http://localhost:5000/api/categories/add-category", {
+        method: "POST",
+        body: JSON.stringify(values),
+        headers: { "Content-type": "application/json; chartset=UTF-8" },
+      });
+      message.success("Kategori başarılı bir şekilde eklendi.");
+      form.resetFields();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <ul className="flex gap-4 md:flex-col text-lg">
+      <li className="category-item">
+        <span>Tümü</span>
+      </li>
+      <li className="category-item">
+        <span>Yiyecek</span>
+      </li>
+      <li className="category-item">
+        <span>İçecek</span>
+      </li>
+      <li className="category-item">
+        <span>İçecek</span>
+      </li>
+      <li className="category-item">
+        <span>İçecek</span>
+      </li>
+      <li className="category-item">
+        <span>İçecek</span>
+      </li>
+      <li className="category-item">
+        <span>İçecek</span>
+      </li>
+      <li className="category-item">
+        <span>İçecek</span>
+      </li>
+      <li className="category-item">
+        <span>İçecek</span>
+      </li>
+      <li
+        className="category-item !bg-purple-800 hover:opacity-90"
+        onClick={() => setIsAddModalOpen(true)}
+      >
+        <PlusOutlined className="md:text-2xl" />
+      </li>
+      <Modal
+        title="Yeni Kategori Ekle"
+        open={isAddModalOpen}
+        onCancel={() => setIsAddModalOpen(false)}
+        footer={false}
+      >
+        <Form layout="vertical" onFinish={onFinish} form={form}>
+          <Form.Item
+            name="title"
+            label="Kategori Ekle"
+            rules={[{ required: true, message: "Kategori adı boş geçilemez!" }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item className="flex justify-end mb-0">
+            <Button type="primary" htmlType="submit">
+              Oluştur
+            </Button>
+          </Form.Item>
+        </Form>
+      </Modal>
+    </ul>
+  );
+};
+
+export default Categories;
