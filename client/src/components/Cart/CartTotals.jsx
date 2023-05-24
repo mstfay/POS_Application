ce7dd8a0dc3dcacf6ea78194e801a90e70cab313
mstfay <aymustafa.32@gmail.com>
2023-05-24 13:26:05 +0300

    The process of adding products to the cart with redux has been completed.

--- client/src/components/Cart/CartTotals.jsx
+++ client/src/components/Cart/CartTotals.jsx
@@ -1,44 +1,51 @@
 import React from "react";
 import { Button } from "antd";
-import { ClearOutlined, PlusCircleOutlined, MinusCircleOutlined } from "@ant-design/icons";
+import {
+  ClearOutlined,
+  PlusCircleOutlined,
+  MinusCircleOutlined,
+} from "@ant-design/icons";
+import { useSelector } from "react-redux";
 
 function CartTotals() {
+  const { cartItems } = useSelector((state) => state.cart);
+
   return (
     <div className="cart h-full max-h-[calc(100vh_-_90px)] flex flex-col">
       <h2 className="bg-blue-600 text-center py-4 text-white font-bold tracking-wide">
         Sepetteki Ürünler
       </h2>
       <ul className="cart-items px-2 flex flex-col gap-y-3 py-2 overflow-y-auto">
-        <li className="cart-item flex justify-between">
-          <div className="flex items-center">
-            <img
-              src="https://img.piri.net/resim/imagecrop/2020/09/26/12/21/resized_dcc79-24191133elma.jpg"
-              alt=""
-              className="w-16 h-16 object-cover"
-            />
-            <div className="flex flex-col ml-2">
-              <b>Elma</b>
-              <span>12₺ x 2</span>
+        {cartItems.map((item) => (
+          <li className="cart-item flex justify-between" key={item._id}>
+            <div className="flex items-center">
+              <img src={item.image} alt="" className="w-16 h-16 object-cover" />
+              <div className="flex flex-col ml-2">
+                <b>{item.title}</b>
+                <span>
+                  {item.price}₺ x {item.quantity}
+                </span>
+              </div>
             </div>
-          </div>
-          <div className="flex items-center gap-x-1">
-            <Button
-              type="primary"
-              size="small"
-              className="w-full flex items-center justify-center !rounded-full"
-              icon={<PlusCircleOutlined />}
-              danger
-            />
-            <span className="font-bold">1</span>
-            <Button
-              type="primary"
-              size="small"
-              className="w-full flex items-center justify-center !rounded-full"
-              icon={<MinusCircleOutlined />}
-              danger
-            />
-          </div>
-        </li>
+            <div className="flex items-center gap-x-1">
+              <Button
+                type="primary"
+                size="small"
+                className="w-full flex items-center justify-center !rounded-full"
+                icon={<PlusCircleOutlined />}
+                danger
+              />
+              <span className="font-bold">{item.quantity}</span>
+              <Button
+                type="primary"
+                size="small"
+                className="w-full flex items-center justify-center !rounded-full"
+                icon={<MinusCircleOutlined />}
+                danger
+              />
+            </div>
+          </li>
+        ))}
       </ul>
       <div className="cart-totals mt-auto">
         <div className="border-t border-b">
